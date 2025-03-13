781871c4ce49afbf3b7aaee4d32c7965a78176d2
Marina <climovicimarina@gmail.com>
2025-03-13 15:10:54 +0100

    model and serializer

--- user_tracking/location/models.py
+++ user_tracking/location/models.py
@@ -1,2 +1,12 @@
-from django.db import models
+from django.contrib.gis.db import models
 from django.contrib.auth.models import User
+
+class UserLocation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.PointField(geography=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager
+
+    def __str__(self):
+        return f"User: {self.user}, location: {self.location}, last_update: {self.last_update}"
---
+++ user_tracking/location/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.gis.geos import Point
+from .models import UserLocation
+
+
+class UserLocationSerializer(serializers.ModelSerializer):
+    longtitude = serializers.FloatField(write_only=True)
+    latitude = serializers.FloatField(write_only=True)
+
+    class Meta:
+        model = UserLocation
+        fields = ["user", "longtitude", "latitude", "last_update"]
+        read_only_field = ["last_update"]
+
+    def create(self, validated_data):
+        longtitude = validated_data.pop("longtitude")
+        latitude = validated_data.pop("latitude")
+        user = validated_data.get("user")
+        point = Point(longtitude, latitude, srid=4326)
+
+        try:
+            user_location = UserLocation.objects.get(user=user)
+            user_location.location = point
+            user_location.save()
+            return user_location
+        except UserLocation.DoesNotExist:
+            user_location = UserLocation.objects.create(user=user, location=point)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["longtitude"] = instance.location.x
+        representation["latitude"] = instance.location.y
+        return representation
